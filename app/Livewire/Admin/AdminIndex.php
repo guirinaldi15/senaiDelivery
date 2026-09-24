bcfea5cc2c9122e8127ad98511cdac60cc1d8705
guirinaldi15 <tremmbala45@gmail.com>
2026-09-24 11:06:24 -0300

    implementação do admin

---
+++ app/Livewire/Admin/AdminIndex.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class AdminIndex extends Component
+{
+    use WithPagination;
+    public $pesquisa = '';
+    public $situacao = 'ativos';
+
+    public function administradores()
+    {
+        $admins = User::where('tipo', User::TIPO_ADMIN);
+        if ($this->situacao == 'excluídos') {
+            $admins->onlyTrashed();
+        } else if ($this->situacao == 'todos') {
+            $admins->withTrashed();
+        }
+
+        $admins->where('nome', 'like', '%'.$this->pesquisa.'%')
+        ->orWhere('email', 'like', '%' .$this->pesquisa.'%');
+
+        return $admins->paginate(10);
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.admin-index')->layout('layouts.app', ['tittle' => 'Administradores', 'admin' => true]);
+    }
+}
